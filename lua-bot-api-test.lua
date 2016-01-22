6cc1edec3e1d1a694972e61370599ad98e9d66c2
cosmonawt <cosmonawt@users.noreply.github.com>
2016-01-22 20:36:40 +0100

    added another example on how to send photos

--- lua-bot-api-test.lua
+++ lua-bot-api-test.lua
@@ -7,9 +7,23 @@ local bot = require("lua-bot-api").configure(token)
 -- get table of updates
 local updates = bot.getUpdates()
 
--- for each update, check wether message is 'ping' and reply accordingly
-for key, value in ipairs(updates.result) do
-  if value.message.text == "ping" then
-    print(bot.sendMessage(value.message.from.id, "pong"))
+-- for each update, check the message
+-- Note: processing a message does not prevent it from appearing again, unless
+-- you feed it's update id (incremented by one) back into getUpdates()
+for key, query in pairs(updates.result) do
+  -- only reply to private chats, not groups
+  if(query.message.chat.type == "private") then
+    -- if message text was 'ping'
+    if query.message.text == "ping" then
+      -- reply with 'pong'
+      print(bot.sendMessage(query.message.from.id, "pong"))
+
+    -- if message text was 'photo'
+    elseif query.message.text == "photo" then
+      -- get the users profile pictures
+      local profilePicture = getUserProfilePhotos(query.message.from.id)
+      -- and send the first one back to him using its file id
+      sendPhoto(query.message.from.id, profilePicture.result.photos[1][1].file_id)
+    end
   end
 end
