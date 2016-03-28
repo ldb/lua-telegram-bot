67b38263099c588b995af4c2cd312ac464d56136
cosmonawt <cosmonawt@users.noreply.github.com>
2016-03-28 04:19:06 +0200

    Updated License to "GPLv2 or Later"

--- lua-bot-api.lua
+++ lua-bot-api.lua
@@ -1,19 +1,25 @@
--- Telegram Bot API Framework for LUA
--- More information on the API: https://core.telegram.org/bots/api
--- Copyright (C) 2016 cosmonawt
+--[[
 
--- This program is free software: you can redistribute it and/or modify
--- it under the terms of the GNU General Public License as published by
--- the Free Software Foundation, either version 3 of the License, or
--- (at your option) any later version.
+lua-bot-api.lua - A Lua library to the Telegram Bot API
+(https://core.telegram.org/bots/api)
 
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU General Public License for more details.
+Copyright (C) 2016 @cosmonawt
 
--- You should have received a copy of the GNU General Public License
--- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+]]
 
 -- Import Libraries
 local https = require("ssl.https")
