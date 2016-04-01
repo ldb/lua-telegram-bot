4f9019c8fb38c5fdb129d57c5727b6636da1ce59
cosmonawt <cosmonawt@users.noreply.github.com>
2016-04-01 08:53:18 +0200

    JSON Error handling
    - Included onDecodeofHTMLError() and onDecodeofNilError() for JSON
    library.
    -Included timestamps in printed error message

--- lua-bot-api.lua
+++ lua-bot-api.lua
@@ -40,11 +40,33 @@ function JSON:onDecodeError(message, text, location, etc)
       message = string.format("%s: %s", message, text)
     end
   end
-  print("Error while decoding JSON:\n", message)
+  print((os.date("%x %X")), "Error while decoding JSON:\n", message)
+end
+
+function JSON:onDecodeOfHTMLError(message, text, nil, etc)
+  if text then
+    if location then
+      message = string.format("%s at char %d of: %s", message, location, text)
+    else
+      message = string.format("%s: %s", message, text)
+    end
+  end
+  print((os.date("%x %X")), "Error while decoding JSON [HTML]:\n", message)
+end
+
+function JSON:onDecodeOfNilError(message, nil, nil, etc)
+  if text then
+    if location then
+      message = string.format("%s at char %d of: %s", message, location, text)
+    else
+      message = string.format("%s: %s", message, text)
+    end
+  end
+  print((os.date("%x %X")), "Error while decoding JSON [nil]:\n", message)
 end
 
 function JSON:onEncodeError(message, etc)
-  print("Error while encoding JSON:\n", message)
+  print((os.date("%x %X")), "Error while encoding JSON:\n", message)
 end
 
 -- configure and initialize bot
